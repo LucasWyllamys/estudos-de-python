cf09bed6dce18df01ba1bc77bf67b174f056eb57
Wyllamys <lucas.wyllamys@outlook.com>
2024-06-26 00:45:00 -0300

    26.06.2024-00:44

---
+++ exemplos-desafios-exercicios/002-tipos-primitivos/01-soma.py
@@ -0,0 +1,4 @@
+num1 = float(input('Primeiro número: '))    #A função float é usada para converter string em número flutuante
+num2 = float(input('Segundo número: '))
+soma = num1 + num2
+print('A soma é', soma)
--- exemplos-desafios-exercicios/01-input-print/script04.py
+++
@@ -1,4 +0,0 @@
-num1 = float(input('Primeiro número: '))
-num2 = float(input('Segundo número: '))
-soma = num1 + num2
-print('A soma é', soma)
