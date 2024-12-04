c5eab6dd48b97b6440fd00a6dc490d91cdd13085
LUCAS WYLLAMYS CARMO DA SILVA <127877422+LucasWyllamys@users.noreply.github.com>
2024-12-04 18:16:03 -0300

    04.12.2024-18:15

--- w3schools/12-regex/metodos-re.py
+++ w3schools/12-regex/metodos-re.py
@@ -1,3 +1,10 @@
+# Uma RegEx, ou Expressão Regular, é uma sequência de caracteres que forma um padrão de pesquisa.
+# O RegEx pode ser usado para verificar se uma string contém o padrão de pesquisa especificado. Com o Regex você consegue:
+    # Realizar buscas por padrões e strings.
+    # Fazer validações de padrões e texto.
+    # Substituições de padõres e strings.
+# O Python tem uma biblioteca integrada chamada re, que pode ser usada para trabalhar com expressões regulares.
+
 import re
 
 string = "Lucas Wyllamys"
