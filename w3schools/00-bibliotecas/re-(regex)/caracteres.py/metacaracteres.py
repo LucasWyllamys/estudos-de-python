c2261c19e7711ee4a0d4c21f42724e63f1be9732
LUCAS WYLLAMYS CARMO DA SILVA <127877422+LucasWyllamys@users.noreply.github.com>
2024-12-11 18:21:21 -0300

    11.12.2024-18:20

--- w3schools/00-bibliotecas/re-(regex)/caracteres.py/metacaracteres.py
+++ w3schools/00-bibliotecas/re-(regex)/caracteres.py/metacaracteres.py
@@ -1,14 +1,14 @@
 ''' Metacaracteres são caracteres com um significado especial:
 Caracter    Descrição
-[]          Um conjunto de caracteres.        
-/           Sinaliza uma sequência especial (também pode ser usado para escapar caracteres especiais).           
+[]          Um conjunto de caracteres (e/ou conjunto de possibilidades).        
+\           Sinaliza uma sequência especial (também pode ser usado para escapar caracteres especiais).           
 .           Representa qualquer caracter, exceto \n.
 ^	        Começa com.	
 $	        Termina com.
-*	        Zero ou mais ocorrências.
-+	        Uma ou mais ocorrências.
-?	        Zero ou uma ocorrência.
-{}	        Exatamente o número especificado de ocorrências.
+*	        Especifica zero ou mais ocorrências.
++	        Especifica uma ou mais ocorrências.
+?	        Especifica zero ou uma ocorrência.
+{}	        Especifica o número de ocorrências.
 |	        Ou.
 ()	        Capturar e agrupar.
 '''
--- w3schools/12-regex/exemplos.py
+++ w3schools/12-regex/exemplos.py
@@ -6,17 +6,22 @@
 # O Python tem um pacote integrado chamado re, que pode ser usado para trabalhar com expressões regulares.
 import re
 
-# Exemplos:--------------------------------------------------------------------
-
-string = "Lucas Wyllamys Carmo da Silva - Lucas Wyllamys"
-
-padrao = re.compile("Lucas Wyllamys")
-
+# Validação de CPF:
+string = "071.393.565-05"
+padrao = re.compile("[0-9]{3}\.[0-9]{3}\.[0-9]{3}-[0-9]{2}")  # Como se lê este padrão: [números de 0 a 9]{3 ocorrências}ponto[números de 0 a 9]{3 ocorrências}ponto[números de 0 a 9]{3 ocorrências}traço[némros de 0 a 9]{2 ocorrências}
 result = re.fullmatch(padrao, string)  
-print(result)   # Saída: None
+print(result) 
 
-result = re.search(padrao, string)
-print(result)   # Saída: <re.Match object; span=(0, 14), match='Lucas Wyllamys'>. O atributo span indica onde que a string começa e termina (o valor de término não entra na conta!).
+# Validação RG:
 
-result = re.findall(padrao, string)
-print(result)   # Saída: ['Lucas Wyllamys', 'Lucas Wyllamys']
+# Validação CNPJ:
+
+# Validação de e-mail:
+string = "lucas.wyllamys@outlook.com"
+padrao = re.compile("[\w\.]+@[\w]+\.[a-zA-Z]+(\.[a-zA-Z]+)?")
+result = re.fullmatch(padrao, string)
+print(result)
+
+# Validação CEP:
+
+# Validação Telefone:
